3e10a64fcd00adb8de7fe8b5b38f534335e59264
Samarión Júnior <samarion2@hotmail.com>
2023-01-13 14:39:38 -0300

    Pagina todo responsiva

--- src/pages/ToDo/index.jsx
+++ src/pages/ToDo/index.jsx
@@ -8,8 +8,54 @@ const ToDo = _ => {
     console.clear()
     const [newTaskInput, setNewTaskInput] = useState("")
     const [tasks, setTasks] = useState([
-        {isChecked: false, name: "a", id: Math.random()},
+        {isChecked: false, name: "fdsghsdfgsfgfgsfgsdfgsdfgsdfgsdfgs", id: Math.random()},
         {isChecked: false, name: "b", id: Math.random()},
+        {isChecked: false, name: "c", id: Math.random()},
+        {isChecked: false, name: "c", id: Math.random()},
+        {isChecked: false, name: "c", id: Math.random()},
+        {isChecked: false, name: "c", id: Math.random()},
+        {isChecked: false, name: "c", id: Math.random()},
+        {isChecked: false, name: "c", id: Math.random()},
+        {isChecked: false, name: "c", id: Math.random()},
+        {isChecked: false, name: "c", id: Math.random()},
+        {isChecked: false, name: "c", id: Math.random()},
+        {isChecked: false, name: "c", id: Math.random()},
+        {isChecked: false, name: "c", id: Math.random()},
+        {isChecked: false, name: "c", id: Math.random()},
+        {isChecked: false, name: "c", id: Math.random()},
+        {isChecked: false, name: "c", id: Math.random()},
+        {isChecked: false, name: "c", id: Math.random()},
+        {isChecked: false, name: "c", id: Math.random()},
+        {isChecked: false, name: "c", id: Math.random()},
+        {isChecked: false, name: "c", id: Math.random()},
+        {isChecked: false, name: "c", id: Math.random()},
+        {isChecked: false, name: "c", id: Math.random()},
+        {isChecked: false, name: "c", id: Math.random()},
+        {isChecked: false, name: "c", id: Math.random()},
+        {isChecked: false, name: "c", id: Math.random()},
+        {isChecked: false, name: "c", id: Math.random()},
+        {isChecked: false, name: "c", id: Math.random()},
+        {isChecked: false, name: "c", id: Math.random()},
+        {isChecked: false, name: "c", id: Math.random()},
+        {isChecked: false, name: "c", id: Math.random()},
+        {isChecked: false, name: "c", id: Math.random()},
+        {isChecked: false, name: "c", id: Math.random()},
+        {isChecked: false, name: "c", id: Math.random()},
+        {isChecked: false, name: "c", id: Math.random()},
+        {isChecked: false, name: "c", id: Math.random()},
+        {isChecked: false, name: "c", id: Math.random()},
+        {isChecked: false, name: "c", id: Math.random()},
+        {isChecked: false, name: "c", id: Math.random()},
+        {isChecked: false, name: "c", id: Math.random()},
+        {isChecked: false, name: "c", id: Math.random()},
+        {isChecked: false, name: "c", id: Math.random()},
+        {isChecked: false, name: "c", id: Math.random()},
+        {isChecked: false, name: "c", id: Math.random()},
+        {isChecked: false, name: "c", id: Math.random()},
+        {isChecked: false, name: "c", id: Math.random()},
+        {isChecked: false, name: "c", id: Math.random()},
+        {isChecked: false, name: "c", id: Math.random()},
+        {isChecked: false, name: "c", id: Math.random()},
         {isChecked: false, name: "c", id: Math.random()},
         {isChecked: false, name: "c", id: Math.random()},
         {isChecked: false, name: "c", id: Math.random()},
@@ -79,35 +125,28 @@ const ToDo = _ => {
     console.log(tasks)
     return (
         <div id="ToDo" className="ToDo">
-            <div className="ToDoContent">
-                <div className="TaskList">
+            <main className="ToDoContent">
+                <section className="List">
                     <div className="Form">
                         <input id="NewTaskInput" name="NewTaskInput" type="text" className="TextInput" placeholder="Digite uma Nova Tarefa?" value={newTaskInput} onChange={(e) => setNewTaskInput(String(e.target.value))}/>
-                        <button className="ButtonTextInput" onClick={() => newTaskInput !== "" ? setNewTask(newTaskInput) : console.log("Vazio!!!")}>Adicionar</button>
+                        <button className="ButtonTextInput" onClick={() => newTaskInput !== "" ? setNewTask(newTaskInput) : console.log("Vazio!!!")}>A</button>
                     </div>
                     <div className="Tasks">
                         {tasks.length !== 0 ? tasks.map(task => 
-                            <div key={String(task.id)} className="Task">
+                            <div key={String(task.id)} className="Items">
                                 <label className="containerCheckBox">
-                                    <input 
-                                        id={`check${task.id}`}
-                                        name={`check${task.id}`}
-                                        type="checkbox"
-                                        className="CheckInput" 
-                                        check={String(task.isChecked)}
-                                        onChange={(e) => Check(Number(task.id))}
-                                    />
+                                    <input id={`check${task.id}`} name={`check${task.id}`} type="checkbox" className="CheckInput"  check={String(task.isChecked)} onChange={(e) => Check(Number(task.id))}/>
                                     <span className="spanChackBox"></span>
                                 </label>
                                 <label htmlFor={`check${task.id}`} className={task.isChecked ? "Label cortado" : "Label"}>{task.name}</label>
-                                <button className="ButtonCheckInput" onClick={(e => RemoveTask(Number(task.id)))}>Remover</button>
+                                <button className="ButtonCheckInput" onClick={(e => RemoveTask(Number(task.id)))}>R</button>
                             </div>
                         ) : 
                             <h1>Vazio!!!</h1>
                         }
                     </div>
-                </div>
-            </div>
+                </section>
+            </main>
         </div>
     )
 }
