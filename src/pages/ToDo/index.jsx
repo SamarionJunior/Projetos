f47feb91ed4dc9f967c72056174b463fe226869c
Samarión Júnior <samarion2@hotmail.com>
2023-01-17 10:31:50 -0300

    Responsividade Mobile PC

--- src/pages/ToDo/index.jsx
+++ src/pages/ToDo/index.jsx
@@ -132,18 +132,42 @@ const ToDo = _ => {
                         <button className="ButtonTextInput" onClick={() => newTaskInput !== "" ? setNewTask(newTaskInput) : console.log("Vazio!!!")}>A</button>
                     </div>
                     <div className="Tasks">
-                        {tasks.length !== 0 ? tasks.map(task => 
-                            <div key={String(task.id)} className="Items">
-                                <label className="containerCheckBox">
-                                    <input id={`check${task.id}`} name={`check${task.id}`} type="checkbox" className="CheckInput"  check={String(task.isChecked)} onChange={(e) => Check(Number(task.id))}/>
-                                    <span className="spanChackBox"></span>
-                                </label>
-                                <label htmlFor={`check${task.id}`} className={task.isChecked ? "Label cortado" : "Label"}>{task.name}</label>
-                                <button className="ButtonCheckInput" onClick={(e => RemoveTask(Number(task.id)))}>R</button>
-                            </div>
-                        ) : 
-                            <h1>Vazio!!!</h1>
-                        }
+                        <div className="Scroll">
+                            {tasks.length !== 0 ? tasks.map(task => 
+                                <div key={String(task.id)} className="Items">
+                                    <label className="containerCheckBox">
+                                        <input id={`check${task.id}`} name={`check${task.id}`} type="checkbox" className="CheckInput"  check={String(task.isChecked)} onChange={(e) => Check(Number(task.id))}/>
+                                        <span className="spanChackBox"></span>
+                                    </label>
+                                    <label htmlFor={`check${task.id}`} className={task.isChecked ? "Label cortado" : "Label"}>{task.name}</label>
+                                    <button className="ButtonCheckInput" onClick={(e => RemoveTask(Number(task.id)))}>R</button>
+                                </div>
+                            ) : 
+                                <h1>Vazio!!!</h1>
+                            }
+                        </div>
+                    </div>
+                </section>
+                <section className="List">
+                    <div className="Form">
+                        <input id="NewTaskInput" name="NewTaskInput" type="text" className="TextInput" placeholder="Digite uma Nova Tarefa?" value={newTaskInput} onChange={(e) => setNewTaskInput(String(e.target.value))}/>
+                        <button className="ButtonTextInput" onClick={() => newTaskInput !== "" ? setNewTask(newTaskInput) : console.log("Vazio!!!")}>A</button>
+                    </div>
+                    <div className="Tasks">
+                        <div className="Scroll">
+                            {tasks.length !== 0 ? tasks.map(task => 
+                                <div key={String(task.id)} className="Items">
+                                    <label className="containerCheckBox">
+                                        <input id={`check${task.id}`} name={`check${task.id}`} type="checkbox" className="CheckInput"  check={String(task.isChecked)} onChange={(e) => Check(Number(task.id))}/>
+                                        <span className="spanChackBox"></span>
+                                    </label>
+                                    <label htmlFor={`check${task.id}`} className={task.isChecked ? "Label cortado" : "Label"}>{task.name}</label>
+                                    <button className="ButtonCheckInput" onClick={(e => RemoveTask(Number(task.id)))}>R</button>
+                                </div>
+                            ) : 
+                                <h1>Vazio!!!</h1>
+                            }
+                        </div>
                     </div>
                 </section>
             </main>
