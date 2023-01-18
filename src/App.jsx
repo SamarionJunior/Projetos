a34b39dfd8d7b6876dc4f80a10bc22dba204db7e
Samarión Júnior <samarion2@hotmail.com>
2023-01-18 15:26:58 -0300

    Responsiivdade 1 Agenda

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,11 @@ import { Routes, Route } from "react-router-dom";
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
-import { faPlus, faTrash, faMagnifyingGlass, faXmark, faArrowsUpDownLeftRight, faPen, faCaretDown} from '@fortawesome/free-solid-svg-icons'
+import { 
+  faPlus, faTrash, faMagnifyingGlass, faXmark,
+  faArrowsUpDownLeftRight, faPen, faCaretDown,
+  faFloppyDisk
+} from '@fortawesome/free-solid-svg-icons'
 
 // import Painel from './pages/Painel';
 // import Gallery from './pages/Gallery';
@@ -14,7 +18,11 @@ import Schedule from "./pages/Schedule";
 import ToDo from "./pages/ToDo";
 import Notepad from "./pages/Notepad";
 
-library.add(fab, faPlus, faTrash, faMagnifyingGlass, faXmark, faArrowsUpDownLeftRight, faPen, faCaretDown)
+library.add(fab,
+  faPlus, faTrash, faMagnifyingGlass, faXmark,
+  faArrowsUpDownLeftRight, faPen, faCaretDown,
+  faFloppyDisk
+)
 
 const App = _ => (
   <div className="App">
--- src/pages/Schedule/index.jsx
+++ src/pages/Schedule/index.jsx
@@ -5,6 +5,8 @@ import React from "react";
 import { useState } from "react";
 import { useEffect } from "react";
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 const Schedule = _ => {
     const [horarioDeInicio, setHorarioDeInicio] = useState("")
     const [horarioDeEncerramento, setHorarioDeEncerramento] = useState("")
@@ -48,23 +50,31 @@ const Schedule = _ => {
     return (
         <div id="Schedule" className="Schedule">
             <div className="ScheduleContent">
-                <div className="Day">
-                    {eventos.eventos.map(evento => 
-                        <div key={evento.id} className="Event">
-                            <input type="time" className="Hour" value={evento.horarioDeInicio} onChange={(e) => Update(evento.id, {"horarioDeInicio": String(e.target.value)})}/>
-                            <input type="time" className="Hour" value={evento.horarioDeEncerramento} onChange={(e) => Update(evento.id, {"horarioDeEncerramento": String(e.target.value)})}/>
-                            <input type="text" className="EventText" value={evento.nome} onChange={(e) => Update(evento.id, {"nome": String(e.target.value)})}/>
-                            <button className="EventDelete" onClick={() => Delete(evento.id)}>
-                                Delete
-                            </button>
+                <div className="ScheduleDay BoxShadow">
+                    <div className="ScheduleFrom">
+                        <input type="time" className="ScheduleInputHour" value={horarioDeInicio} onChange={(e) => setHorarioDeInicio(String(e.target.value))}/>
+                        <input type="time" className="ScheduleInputHour" value={horarioDeEncerramento} onChange={(e) => setHorarioDeEncerramento(String(e.target.value))}/>
+                        <input type="text" className="ScheduleTextInput" value={nome} onChange={(e) => setNome(String(e.target.value))} placeholder="Digite um Novo Evento!!!" />
+                        <button type="button" className="ScheduleButtonTextInput" value="Clear" onClick={() => Clear()}>
+                            <FontAwesomeIcon icon="fa-xmark" />
+                        </button>
+                        <button type="button" className="ScheduleButtonTextInput" value="Clear" onClick={() => Insert()}>
+                            <FontAwesomeIcon icon="fa-floppy-disk" />
+                        </button>
+                    </div>
+                    <div className="ScheduleEvents">
+                        <div className="ScheduleScroll">
+                            {eventos.eventos.map(evento => 
+                                <div key={evento.id} className="ScheduleEvent">
+                                    <input type="time" className="Hour" value={evento.horarioDeInicio} onChange={(e) => Update(evento.id, {"horarioDeInicio": String(e.target.value)})}/>
+                                    <input type="time" className="Hour" value={evento.horarioDeEncerramento} onChange={(e) => Update(evento.id, {"horarioDeEncerramento": String(e.target.value)})}/>
+                                    <input type="text" className="ScheduleLabel" value={evento.nome} onChange={(e) => Update(evento.id, {"nome": String(e.target.value)})}/>
+                                    <button className="ScheduleButtonCheckInput" onClick={() => Delete(evento.id)}>
+                                        <FontAwesomeIcon icon="fa-trash" />
+                                    </button>
+                                </div>
+                            )}
                         </div>
-                    )}
-                    <div className="Input">
-                        <input type="time" className="InputHour" value={horarioDeInicio} onChange={(e) => setHorarioDeInicio(String(e.target.value))}/>
-                        <input type="time" className="InputHour" value={horarioDeEncerramento} onChange={(e) => setHorarioDeEncerramento(String(e.target.value))}/>
-                        <input type="text" className="InputText" value={nome} onChange={(e) => setNome(String(e.target.value))} placeholder="Digite um Novo Evento!!!" />
-                        <input type="button" className="InputButton" value="Save" onClick={() => Insert()}/>
-                        <input type="button" className="InputButton" value="Clear" onClick={() => Clear()}/>
                     </div>
                 </div>
             </div>
