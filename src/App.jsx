124d642781a5d8c8cfc82fc0cb3e89ac4270ffee
Samarión Júnior <samarion2@hotmail.com>
2023-02-14 17:08:10 -0300

    Atualizando layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,13 @@ import "./style.css";
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-import { 
-  faPlus, faTrash, faMagnifyingGlass, faXmark,
-  faArrowsUpDownLeftRight, faPen, faCaretDown,
-  faFloppyDisk
-} from '@fortawesome/free-solid-svg-icons'
+// import { library } from '@fortawesome/fontawesome-svg-core'
+// import { fab } from '@fortawesome/free-brands-svg-icons'
+// import { 
+//   faPlus, faTrash, faMagnifyingGlass, faXmark,
+//   faArrowsUpDownLeftRight, faPen, faCaretDown,
+//   faFloppyDisk, faBroom
+// } from '@fortawesome/free-solid-svg-icons'
 
 // import Painel from './pages/Painel';
 // import Gallery from './pages/Gallery';
@@ -18,19 +18,19 @@ import Schedule from "./pages/Schedule";
 import ToDo from "./pages/ToDo";
 import Notepad from "./pages/Notepad";
 
-library.add(fab,
-  faPlus, faTrash, faMagnifyingGlass, faXmark,
-  faArrowsUpDownLeftRight, faPen, faCaretDown,
-  faFloppyDisk
-)
+// library.add(fab,
+//   faPlus, faTrash, faMagnifyingGlass, faXmark,
+//   faArrowsUpDownLeftRight, faPen, faCaretDown,
+//   faFloppyDisk, faBroom
+// )
 
 const App = _ => (
   <div className="App">
     <Routes>
       {/* <Route path="painel" element={<Painel />} /> */}
       <Route path="agenda" element={<Schedule />} />
-      <Route path="todo" element={<ToDo />} />
-      <Route path="/" element={<Notepad />} />
+      <Route path="/" element={<ToDo />} />
+      <Route path="notepad" element={<Notepad />} />
     </Routes>
   </div>
 )
--- src/pages/ToDo/index.jsx
+++ src/pages/ToDo/index.jsx
@@ -1,22 +1,28 @@
 import "../../style.css"
 import "./style.css"
 
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const ToDo = _ => {
     console.clear()
     const [newTaskInput, setNewTaskInput] = useState("")
     const [tasks, setTasks] = useState([
-        {isChecked: false, name: "fdsghsdfgsfgfgsfg344535234525634636456345635sdfgsdfgsdfgsdfgs", id: Math.random()},
-        {isChecked: false, name: "b", id: Math.random()},
-        {isChecked: false, name: "c", id: Math.random()},
-        {isChecked: false, name: "c", id: Math.random()},
-        {isChecked: false, name: "c", id: Math.random()},
-        {isChecked: false, name: "c", id: Math.random()},
-        {isChecked: false, name: "c", id: Math.random()},
+        {isChecked: false, name: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus cupiditate accusantium vero, odio fugit nemo. Commodi hic rem architecto quae, sapiente perspiciatis iure quas inventore neque, tempora beatae amet aliquid.", id: Math.random()},
+        {isChecked: false, name: "qwerqwrqweqwegfwqerwetwetsdtsetwertwertwertwertertwertfwertwertre", id: Math.random()},
+        {isChecked: false, name: "sdfgbsdfgsdfgsdfgdgdfwiuygiufcbasgyfgwrefbgrfyhsigfysrgvfbsuifdgvysd", id: Math.random()},
+        {isChecked: false, name: "sdruiwhoeiurgwheruwgerhgwuyehgweyghweoiugeghweuighwoegweugowehgu", id: Math.random()},
+        {isChecked: false, name: "rwhenunwehwuhrsjhlrwjehgbgxdflngvwehrugwhergvnwergmwejghwerjgwergwergwe", id: Math.random()},
+        {isChecked: false, name: "uhjrwebhgewryhgweyrghbwehgwbreywcncwyhrvnhevuerwervbkhwejrvnwekvhervjhwe", id: Math.random()},
+        {isChecked: false, name: "weruiuethwegieghweyrwhgeryweeeeeeeeeehrwfgbrnhwfergrywiugryqwguehuqie", id: Math.random()},
+        // {isChecked: false, name: "b", id: Math.random()},
+        // {isChecked: false, name: "c", id: Math.random()},
+        // {isChecked: false, name: "c", id: Math.random()},
+        // {isChecked: false, name: "c", id: Math.random()},
+        // {isChecked: false, name: "c", id: Math.random()},
+        // {isChecked: false, name: "c", id: Math.random()},
         // {isChecked: false, name: "c", id: Math.random()},
         // {isChecked: false, name: "c", id: Math.random()},
         // {isChecked: false, name: "c", id: Math.random()},
@@ -112,6 +118,8 @@ const ToDo = _ => {
         )
         setNewTaskInput("")
     }
+    const [dataList, setDataList] = useState(tasks)
+    const [searchList, setSearchList] = useState(false)
     const Check = id => setTasks(tasks.map(task => {
         if(task.id === id){
             task.isChecked = !task.isChecked
@@ -124,45 +132,100 @@ const ToDo = _ => {
         }
         return true
     }))
+    const Search = _ => {
+        setSearchList(true)
+        setDataList(tasks.map(task => {
+            if(task.name.includes(newTaskInput)){
+                return task
+            }
+        }))
+    }
+    useEffect(() => {
+        if(!searchList){
+            setDataList(tasks)
+            console.log("setDataList")
+        }
+    }, [tasks, searchList])
+    const FullText = (command, id) => {
+        if(command){
+            document.getElementById(`Label${id}`).style.whiteSpace = "normal"
+            document.getElementById(`Items${id}`).style.alignItems = "start"
+            document.getElementById(`FullTextButtonOpen${id}`).style.display = "none"
+            document.getElementById(`FullTextButtonClose${id}`).style.display = "block"
+        }else{
+            document.getElementById(`Label${id}`).style.whiteSpace = "nowrap"
+            document.getElementById(`Items${id}`).style.alignItems = "center"
+            document.getElementById(`FullTextButtonOpen${id}`).style.display = "block"
+            document.getElementById(`FullTextButtonClose${id}`).style.display = "none"
+        }
+    }
+    // useEffect(() => {
+    //     setShow(dataList.length !== 0 ? dataList.map(task => 
+    //         <div key={String(task.id)} className="Items">
+    //             <input id={`check${task.id}`} name={`check${task.id}`} type="checkbox" className="CheckInput"  check={String(task.isChecked)} onChange={(e) => Check(Number(task.id))}/>
+    //             <button className="ButtonCheckInput" onClick={(e => RemoveTask(Number(task.id)))}>
+    //                 <FontAwesomeIcon icon="fa-caret-down" />
+    //             </button>
+    //             <label htmlFor={`check${task.id}`} className={task.isChecked ? "Label cortado" : "Label"}>{task.name}</label>
+    //             <button className="ButtonCheckInput" onClick={(e => RemoveTask(Number(task.id)))}>
+    //                 <FontAwesomeIcon icon="fa-arrows-up-down-left-right" />
+    //             </button>
+    //             <button className="ButtonCheckInput" onClick={(e => RemoveTask(Number(task.id)))}>
+    //                 <FontAwesomeIcon icon="fa-pen" />
+    //             </button>
+    //             <button className="ButtonCheckInput" onClick={(e => RemoveTask(Number(task.id)))}>
+    //                 <FontAwesomeIcon icon="fa-trash" />
+    //             </button>
+    //         </div>
+    //     ) : <h1>Vazio!!!</h1>)
+    // }, [dataList])
     console.log(tasks)
     return (
         <div id="ToDo" className="ToDo">
             <main className="ToDoContent">
                 <section className="List BoxShadow">
                     <div className="Form">
-                        <button className="ButtonTextInput" onClick={() => newTaskInput !== "" ? setNewTask(newTaskInput) : console.log("Vazio!!!")}>
-                            <FontAwesomeIcon icon="fa-magnifying-glass" />
+                        <button className="ButtonTextInput" onClick={() => newTaskInput !== "" ? Search() : console.log("Vazio!!!")}>
+                            <i className="fa-solid fa-magnifying-glass" />
                         </button>
+
+                        <button className="ButtonTextInput" onClick={() => searchList !== false ? setSearchList(false) : undefined}>
+                            <i className="fa-solid fa-xmark" />
+                        </button>
+
                         <input id="NewTaskInput" name="NewTaskInput" type="text" className="TextInput" placeholder="Digite uma Nova Tarefa?" value={newTaskInput} onChange={(e) => setNewTaskInput(String(e.target.value))}/>
-                        <button className="ButtonTextInput" onClick={() => newTaskInput !== "" ? setNewTask(newTaskInput) : console.log("Vazio!!!")}>
-                            <FontAwesomeIcon icon="fa-xmark" />
+                       
+                        <button className="ButtonTextInput" onClick={() => newTaskInput !== "" ? setNewTaskInput("") : console.log("Vazio!!!")}>
+                            <i className="fa-solid fa-broom"></i>
                         </button>
+
                         <button className="ButtonTextInput" onClick={() => newTaskInput !== "" ? setNewTask(newTaskInput) : console.log("Vazio!!!")}>
-                            <FontAwesomeIcon icon="fa-plus" />
+                            <i className="fa-solid fa-plus" />
                         </button>
                     </div>
                     <div className="Tasks">
                         <div className="Scroll">
-                            {tasks.length !== 0 ? tasks.map(task => 
-                                <div key={String(task.id)} className="Items">
+                            {dataList.length !== 0 ? dataList.map(task => 
+                                task !== undefined ? <div key={String(task.id)} className="Items" id={`Items${task.id}`} >
                                     <input id={`check${task.id}`} name={`check${task.id}`} type="checkbox" className="CheckInput"  check={String(task.isChecked)} onChange={(e) => Check(Number(task.id))}/>
-                                    <button className="ButtonCheckInput" onClick={(e => RemoveTask(Number(task.id)))}>
-                                        <FontAwesomeIcon icon="fa-caret-down" />
+                                    <button className="ButtonCheckInput" id={`FullTextButtonOpen${task.id}`} onClick={(e => FullText(true, Number(task.id)))}>
+                                        <i className="fa-solid fa-caret-down" />
                                     </button>
-                                    <label htmlFor={`check${task.id}`} className={task.isChecked ? "Label cortado" : "Label"}>{task.name}</label>
+                                    <button className="ButtonCheckInput" id={`FullTextButtonClose${task.id}`} style={{display: "none"}} onClick={(e => FullText(false, Number(task.id)))}>
+                                        <i className="fa-solid fa-caret-up" />
+                                    </button>
+                                    <label htmlFor={`check${task.id}`} id={`Label${task.id}`}  className={task.isChecked ? "Label cortado" : "Label"}>{task.name}</label>
                                     <button className="ButtonCheckInput" onClick={(e => RemoveTask(Number(task.id)))}>
-                                        <FontAwesomeIcon icon="fa-arrows-up-down-left-right" />
+                                        <i className="fa-solid fa-arrows-up-down-left-right" />
                                     </button>
                                     <button className="ButtonCheckInput" onClick={(e => RemoveTask(Number(task.id)))}>
-                                        <FontAwesomeIcon icon="fa-pen" />
+                                        <i className="fa-solid fa-pen" />
                                     </button>
                                     <button className="ButtonCheckInput" onClick={(e => RemoveTask(Number(task.id)))}>
-                                        <FontAwesomeIcon icon="fa-trash" />
+                                        <i className="fa-solid fa-trash" />
                                     </button>
-                                </div>
-                            ) : 
-                                <h1>Vazio!!!</h1>
-                            }
+                                </div> : undefined
+                            ) : <h1>Vazio!!!</h1>}
                         </div>
                     </div>
                 </section>
