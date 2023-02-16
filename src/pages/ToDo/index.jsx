ac9f8a947dcff2aba61e1da7e9bf2075b9452c1e
Samarión Júnior <samarion2@hotmail.com>
2023-02-16 15:38:08 -0300

    todo finalizao

--- src/pages/ToDo/index.jsx
+++ src/pages/ToDo/index.jsx
@@ -12,44 +12,33 @@ const ToDo = _ => {
     const [editedText, setEditedText] = useState("")
     const [tasks, setTasks] = useState([
         {isChecked: false, name: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus cupiditate accusantium vero, odio fugit nemo. Commodi hic rem architecto quae, sapiente perspiciatis iure quas inventore neque, tempora beatae amet aliquid.", id: Math.random()},
-        {isChecked: false, name: "qwerqwrqweqwegfwqerwetwetsdtsetwertwertwertwertertwertfwertwertre", id: Math.random()},
-        {isChecked: false, name: "sdfgbsdfgsdfgsdfgdgdfwiuygiufcbasgyfgwrefbgrfyhsigfysrgvfbsuifdgvysd", id: Math.random()},
-        {isChecked: false, name: "sdruiwhoeiurgwheruwgerhgwuyehgweyghweoiugeghweuighwoegweugowehgu", id: Math.random()},
-        {isChecked: false, name: "rwhenunwehwuhrsjhlrwjehgbgxdflngvwehrugwhergvnwergmwejghwerjgwergwergwe", id: Math.random()},
-        {isChecked: false, name: "uhjrwebhgewryhgweyrghbwehgwbreywcncwyhrvnhevuerwervbkhwejrvnwekvhervjhwe", id: Math.random()},
-        {isChecked: false, name: "weruiuethwegieghweyrwhgeryweeeeeeeeeehrwfgbrnhwfergrywiugryqwguehuqie", id: Math.random()},
-        {isChecked: false, name: "sdruiwhoeiurgwheruwgerhgwuyehgweyghweoiugeghweuighwoegweugowehgu", id: Math.random()},
-        {isChecked: false, name: "rwhenunwehwuhrsjhlrwjehgbgxdflngvwehrugwhergvnwergmwejghwerjgwergwergwe", id: Math.random()},
-        {isChecked: false, name: "uhjrwebhgewryhgweyrghbwehgwbreywcncwyhrvnhevuerwervbkhwejrvnwekvhervjhwe", id: Math.random()},
-        {isChecked: false, name: "weruiuethwegieghweyrwhgeryweeeeeeeeeehrwfgbrnhwfergrywiugryqwguehuqie", id: Math.random()},
-        {isChecked: false, name: "sdruiwhoeiurgwheruwgerhgwuyehgweyghweoiugeghweuighwoegweugowehgu", id: Math.random()},
-        {isChecked: false, name: "rwhenunwehwuhrsjhlrwjehgbgxdflngvwehrugwhergvnwergmwejghwerjgwergwergwe", id: Math.random()},
-        {isChecked: false, name: "uhjrwebhgewryhgweyrghbwehgwbreywcncwyhrvnhevuerwervbkhwejrvnwekvhervjhwe", id: Math.random()},
-        {isChecked: false, name: "weruiuethwegieghweyrwhgeryweeeeeeeeeehrwfgbrnhwfergrywiugryqwguehuqie", id: Math.random()},
-        {isChecked: false, name: "sdruiwhoeiurgwheruwgerhgwuyehgweyghweoiugeghweuighwoegweugowehgu", id: Math.random()},
-        {isChecked: false, name: "rwhenunwehwuhrsjhlrwjehgbgxdflngvwehrugwhergvnwergmwejghwerjgwergwergwe", id: Math.random()},
-        {isChecked: false, name: "uhjrwebhgewryhgweyrghbwehgwbreywcncwyhrvnhevuerwervbkhwejrvnwekvhervjhwe", id: Math.random()},
-        {isChecked: false, name: "weruiuethwegieghweyrwhgeryweeeeeeeeeehrwfgbrnhwfergrywiugryqwguehuqie", id: Math.random()},
-        {isChecked: false, name: "sdruiwhoeiurgwheruwgerhgwuyehgweyghweoiugeghweuighwoegweugowehgu", id: Math.random()},
-        {isChecked: false, name: "rwhenunwehwuhrsjhlrwjehgbgxdflngvwehrugwhergvnwergmwejghwerjgwergwergwe", id: Math.random()},
-        {isChecked: false, name: "uhjrwebhgewryhgweyrghbwehgwbreywcncwyhrvnhevuerwervbkhwejrvnwekvhervjhwe", id: Math.random()},
-        {isChecked: false, name: "weruiuethwegieghweyrwhgeryweeeeeeeeeehrwfgbrnhwfergrywiugryqwguehuqie", id: Math.random()},
-        {isChecked: false, name: "sdruiwhoeiurgwheruwgerhgwuyehgweyghweoiugeghweuighwoegweugowehgu", id: Math.random()},
-        {isChecked: false, name: "rwhenunwehwuhrsjhlrwjehgbgxdflngvwehrugwhergvnwergmwejghwerjgwergwergwe", id: Math.random()},
-        {isChecked: false, name: "uhjrwebhgewryhgweyrghbwehgwbreywcncwyhrvnhevuerwervbkhwejrvnwekvhervjhwe", id: Math.random()},
-        {isChecked: false, name: "weruiuethwegieghweyrwhgeryweeeeeeeeeehrwfgbrnhwfergrywiugryqwguehuqie", id: Math.random()},
-        {isChecked: false, name: "sdruiwhoeiurgwheruwgerhgwuyehgweyghweoiugeghweuighwoegweugowehgu", id: Math.random()},
-        {isChecked: false, name: "rwhenunwehwuhrsjhlrwjehgbgxdflngvwehrugwhergvnwergmwejghwerjgwergwergwe", id: Math.random()},
-        {isChecked: false, name: "uhjrwebhgewryhgweyrghbwehgwbreywcncwyhrvnhevuerwervbkhwejrvnwekvhervjhwe", id: Math.random()},
-        {isChecked: false, name: "weruiuethwegieghweyrwhgeryweeeeeeeeeehrwfgbrnhwfergrywiugryqwguehuqie", id: Math.random()},
-        {isChecked: false, name: "sdruiwhoeiurgwheruwgerhgwuyehgweyghweoiugeghweuighwoegweugowehgu", id: Math.random()},
-        {isChecked: false, name: "rwhenunwehwuhrsjhlrwjehgbgxdflngvwehrugwhergvnwergmwejghwerjgwergwergwe", id: Math.random()},
-        {isChecked: false, name: "uhjrwebhgewryhgweyrghbwehgwbreywcncwyhrvnhevuerwervbkhwejrvnwekvhervjhwe", id: Math.random()},
-        {isChecked: false, name: "weruiuethwegieghweyrwhgeryweeeeeeeeeehrwfgbrnhwfergrywiugryqwguehuqie", id: Math.random()},
-        {isChecked: false, name: "sdruiwhoeiurgwheruwgerhgwuyehgweyghweoiugeghweuighwoegweugowehgu", id: Math.random()},
-        {isChecked: false, name: "rwhenunwehwuhrsjhlrwjehgbgxdflngvwehrugwhergvnwergmwejghwerjgwergwergwe", id: Math.random()},
-        {isChecked: false, name: "uhjrwebhgewryhgweyrghbwehgwbreywcncwyhrvnhevuerwervbkhwejrvnwekvhervjhwe", id: Math.random()},
-        {isChecked: false, name: "weruiuethwegieghweyrwhgeryweeeeeeeeeehrwfgbrnhwfergrywiugryqwguehuqie", id: Math.random()},
+        {isChecked: false, name: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus cupiditate accusantium vero, odio fugit nemo. Commodi hic rem architecto quae, sapiente perspiciatis iure quas inventore neque, tempora beatae amet aliquid.", id: Math.random()},
+        {isChecked: false, name: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus cupiditate accusantium vero, odio fugit nemo. Commodi hic rem architecto quae, sapiente perspiciatis iure quas inventore neque, tempora beatae amet aliquid.", id: Math.random()},
+        {isChecked: false, name: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus cupiditate accusantium vero, odio fugit nemo. Commodi hic rem architecto quae, sapiente perspiciatis iure quas inventore neque, tempora beatae amet aliquid.", id: Math.random()},
+        {isChecked: false, name: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus cupiditate accusantium vero, odio fugit nemo. Commodi hic rem architecto quae, sapiente perspiciatis iure quas inventore neque, tempora beatae amet aliquid.", id: Math.random()},
+        {isChecked: false, name: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus cupiditate accusantium vero, odio fugit nemo. Commodi hic rem architecto quae, sapiente perspiciatis iure quas inventore neque, tempora beatae amet aliquid.", id: Math.random()},
+        {isChecked: false, name: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus cupiditate accusantium vero, odio fugit nemo. Commodi hic rem architecto quae, sapiente perspiciatis iure quas inventore neque, tempora beatae amet aliquid.", id: Math.random()},
+        {isChecked: false, name: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus cupiditate accusantium vero, odio fugit nemo. Commodi hic rem architecto quae, sapiente perspiciatis iure quas inventore neque, tempora beatae amet aliquid.", id: Math.random()},
+        {isChecked: false, name: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus cupiditate accusantium vero, odio fugit nemo. Commodi hic rem architecto quae, sapiente perspiciatis iure quas inventore neque, tempora beatae amet aliquid.", id: Math.random()},
+        {isChecked: false, name: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus cupiditate accusantium vero, odio fugit nemo. Commodi hic rem architecto quae, sapiente perspiciatis iure quas inventore neque, tempora beatae amet aliquid.", id: Math.random()},
+        {isChecked: false, name: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus cupiditate accusantium vero, odio fugit nemo. Commodi hic rem architecto quae, sapiente perspiciatis iure quas inventore neque, tempora beatae amet aliquid.", id: Math.random()},
+        {isChecked: false, name: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus cupiditate accusantium vero, odio fugit nemo. Commodi hic rem architecto quae, sapiente perspiciatis iure quas inventore neque, tempora beatae amet aliquid.", id: Math.random()},
+        {isChecked: false, name: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus cupiditate accusantium vero, odio fugit nemo. Commodi hic rem architecto quae, sapiente perspiciatis iure quas inventore neque, tempora beatae amet aliquid.", id: Math.random()},
+        {isChecked: false, name: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus cupiditate accusantium vero, odio fugit nemo. Commodi hic rem architecto quae, sapiente perspiciatis iure quas inventore neque, tempora beatae amet aliquid.", id: Math.random()},
+        {isChecked: false, name: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus cupiditate accusantium vero, odio fugit nemo. Commodi hic rem architecto quae, sapiente perspiciatis iure quas inventore neque, tempora beatae amet aliquid.", id: Math.random()},
+        {isChecked: false, name: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus cupiditate accusantium vero, odio fugit nemo. Commodi hic rem architecto quae, sapiente perspiciatis iure quas inventore neque, tempora beatae amet aliquid.", id: Math.random()},
+        {isChecked: false, name: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus cupiditate accusantium vero, odio fugit nemo. Commodi hic rem architecto quae, sapiente perspiciatis iure quas inventore neque, tempora beatae amet aliquid.", id: Math.random()},
+        {isChecked: false, name: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus cupiditate accusantium vero, odio fugit nemo. Commodi hic rem architecto quae, sapiente perspiciatis iure quas inventore neque, tempora beatae amet aliquid.", id: Math.random()},
+        {isChecked: false, name: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus cupiditate accusantium vero, odio fugit nemo. Commodi hic rem architecto quae, sapiente perspiciatis iure quas inventore neque, tempora beatae amet aliquid.", id: Math.random()},
+        {isChecked: false, name: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus cupiditate accusantium vero, odio fugit nemo. Commodi hic rem architecto quae, sapiente perspiciatis iure quas inventore neque, tempora beatae amet aliquid.", id: Math.random()},
+        {isChecked: false, name: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus cupiditate accusantium vero, odio fugit nemo. Commodi hic rem architecto quae, sapiente perspiciatis iure quas inventore neque, tempora beatae amet aliquid.", id: Math.random()},
+        {isChecked: false, name: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus cupiditate accusantium vero, odio fugit nemo. Commodi hic rem architecto quae, sapiente perspiciatis iure quas inventore neque, tempora beatae amet aliquid.", id: Math.random()},
+        {isChecked: false, name: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus cupiditate accusantium vero, odio fugit nemo. Commodi hic rem architecto quae, sapiente perspiciatis iure quas inventore neque, tempora beatae amet aliquid.", id: Math.random()},
+        {isChecked: false, name: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus cupiditate accusantium vero, odio fugit nemo. Commodi hic rem architecto quae, sapiente perspiciatis iure quas inventore neque, tempora beatae amet aliquid.", id: Math.random()},
+        {isChecked: false, name: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus cupiditate accusantium vero, odio fugit nemo. Commodi hic rem architecto quae, sapiente perspiciatis iure quas inventore neque, tempora beatae amet aliquid.", id: Math.random()},
+        {isChecked: false, name: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus cupiditate accusantium vero, odio fugit nemo. Commodi hic rem architecto quae, sapiente perspiciatis iure quas inventore neque, tempora beatae amet aliquid.", id: Math.random()},
+        {isChecked: false, name: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus cupiditate accusantium vero, odio fugit nemo. Commodi hic rem architecto quae, sapiente perspiciatis iure quas inventore neque, tempora beatae amet aliquid.", id: Math.random()},
+        {isChecked: false, name: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus cupiditate accusantium vero, odio fugit nemo. Commodi hic rem architecto quae, sapiente perspiciatis iure quas inventore neque, tempora beatae amet aliquid.", id: Math.random()},
     ])
     const setNewTask = task => {
         setTasks(oldTasks => 
@@ -165,11 +154,11 @@ const ToDo = _ => {
                             <i className="fa-solid fa-magnifying-glass" />
                         </button>
 
-                        {/* <button className="ButtonTextInput" onClick={() => searchList !== false ? setSearchList(false) : undefined}>
+                        <button className="ButtonTextInput" onClick={() => searchList !== false ? setSearchList(false) : undefined}>
                             <i className="fa-solid fa-xmark" />
-                        </button> */}
+                        </button>
 
-                        <input id="NewTaskInput" name="NewTaskInput" type="text" className="TextInput" placeholder="Digite uma Nova Tarefa?" value={newTaskInput} onChange={(e) => setNewTaskInput(String(e.target.value))}/>
+                        <input id="NewTaskInput" name="NewTaskInput" type="text" className="TextInput" placeholder="Digite uma Nova Tarefa!" value={newTaskInput} onChange={(e) => setNewTaskInput(String(e.target.value))}/>
                        
                         <button className="ButtonTextInput" onClick={() => newTaskInput !== "" ? setNewTaskInput("") : console.log("Vazio!!!")}>
                             <i className="fa-solid fa-broom"></i>
@@ -182,31 +171,32 @@ const ToDo = _ => {
                     <div className="Tasks">
                         <div className="Scroll">
                             {dataList.length !== 0 ? dataList.map(task => 
-                                task !== undefined ? <div key={String(task.id)} className="Items" id={`Items${task.id}`} >
+                                task !== undefined ?
+                                <div /* draggable="true" */ key={String(task.id)} className="Items" id={`Items${task.id}`} >
                                     <input id={`check${task.id}`} name={`check${task.id}`} type="checkbox" className="CheckInput"  check={String(task.isChecked)} onChange={(e) => Check(Number(task.id))}/>
 
-                                    <p htmlFor={`check${task.id}`} id={`LabelLock${task.id}`} className={task.isChecked ? "Label cortado" : "Label"}>{task.name}</p>
-                                    <input htmlFor={`check${task.id}`} id={`LabelUnlock${task.id}`} className="Label" style={{display: "none"}} onChange={(e) => setEditedText(String(e.target.value))} value={editedText}/>
-                                    
-                                    {/* <button className="ButtonCheckInput" id={`FullTextButtonOpen${task.id}`} onClick={(e => FullText(true, Number(task.id)))}>
+                                    <button className="ButtonCheckInput" id={`FullTextButtonOpen${task.id}`} onClick={(e => FullText(true, Number(task.id)))}>
                                         <i className="fa-solid fa-caret-down" />
                                     </button>
                                     <button className="ButtonCheckInput" id={`FullTextButtonClose${task.id}`} style={{display: "none"}} onClick={(e => FullText(false, Number(task.id)))}>
                                         <i className="fa-solid fa-caret-up" />
                                     </button>
 
+                                    <p htmlFor={`check${task.id}`} id={`LabelLock${task.id}`} className={task.isChecked ? "Label cortado" : "Label"}>{task.name}</p>
+                                    <input htmlFor={`check${task.id}`} id={`LabelUnlock${task.id}`} className="Label" style={{display: "none"}} onChange={(e) => {setEditedText(String(e.target.value))}} value={editedText}/>
+
                                     <button className="ButtonCheckInput" id={`UpdateTextButtonOpen${task.id}`}  onClick={(e => UpdateText(true, Number(task.id)))}>
                                         <i className="fa-solid fa-pen" />
                                     </button>
                                     <button className="ButtonCheckInput" id={`UpdateTextButtonClose${task.id}`}  style={{display: "none"}} onClick={(e => UpdateText(false, Number(task.id)))}>
                                         <i class="fa-solid fa-floppy-disk"></i>
-                                    </button> */}
+                                    </button>
 
                                     <button className="ButtonCheckInput" onClick={(e => RemoveTask(Number(task.id)))}>
                                         <i className="fa-solid fa-trash" />
                                     </button>
                                 </div> : undefined
-                            ) : <h1>Vazio!!!</h1>}
+                            ) : <h1 className="Warning">Vazio!!!</h1>}
                         </div>
                     </div>
                 </section>
@@ -214,6 +204,43 @@ const ToDo = _ => {
         </div>
     )
 }
+
+// const columns = document.querySelectorAll(".column");
+
+// document.addEventListener("dragstart", (e) => {
+//   e.target.classList.add("dragging");
+// });
+
+// document.addEventListener("dragend", (e) => {
+//   e.target.classList.remove("dragging");
+// });
+
+// columns.forEach((item) => {
+//   item.addEventListener("dragover", (e) => {
+//     const dragging = document.querySelector(".dragging");
+//     const applyAfter = getNewPosition(item, e.clientY);
+
+//     if (applyAfter) {
+//       applyAfter.insertAdjacentElement("afterend", dragging);
+//     } else {
+//       item.prepend(dragging);
+//     }
+//   });
+// });
+
+// function getNewPosition(column, posY) {
+//   const cards = column.querySelectorAll(".item:not(.dragging)");
+//   let result;
+
+//   for (let refer_card of cards) {
+//     const box = refer_card.getBoundingClientRect();
+//     const boxCenterY = box.y + box.height / 2;
+
+//     if (posY >= boxCenterY) result = refer_card;
+//   }
+
+//   return result;
+// }
 
 export default ToDo
 
